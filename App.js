70659a5267c752fd423707d03258b8ba816345a0
TiagoGottardo <Tiago.panizio@hotmail.com>
2021-10-10 12:15:37 -0400

    Botão de adição de mapas criado!

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedbackBase, } from 'react-native';
 
-export default function App() {
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import Axios from "axios";
+
+export default function Upload() {  
+  const [avatar, setAvatar] = useState();
+
+  async function imagePickerCall() {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+      if (status !== "granted") {
+        alert("Nós precisamos dessa permissão.");
+        return;
+      }
+    }
+
+    const data = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All
+    });
+
+    if (data.cancelled) {
+      return;
+    }
+
+    if (!data.uri) {
+      return;
+    }
+
+    setAvatar(data);
+  }
+
+  async function uploadImage() {
+    const data = new FormData();
+
+    data.append("avatar", {
+      uri: avatar.uri,
+      type: avatar.type
+    });
+
+    await Axios.post("http://localhost:3333/files", data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <TouchableOpacity style={styles.addImageButton} onPress={uploadImage}>
+          <Text style={{ color: 'white', fontSize: 20, fontWeight:"bold", marginTop: -2}}>+</Text>
+        </TouchableOpacity>
+    </View> 
   );
 }
 
@@ -15,7 +59,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column-reverse',
+    alignSelf: 'flex-end',
+    marginBottom: 25,
+    marginRight: 25,
   },
+  addImageButton: {
+    width:60,
+    height:60,
+    backgroundColor:"#1E90FF",
+    borderRadius:30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
