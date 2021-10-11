4b5b1e760264245fa931fac3c86fd738378eaee9
TiagoGottardo <Tiago.panizio@hotmail.com>
2021-10-11 17:45:18 -0400

    Expo ejetado!

--- App.js
+++ App.js
@@ -2,53 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedbackBase, } from 'react-native';
 
-import Constants from "expo-constants";
-import * as Permissions from "expo-permissions";
-import * as ImagePicker from "expo-image-picker";
-import Axios from "axios";
 
-export default function Upload() {  
-  const [avatar, setAvatar] = useState();
 
-  async function imagePickerCall() {
-    if (Constants.platform.ios) {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-      if (status !== "granted") {
-        alert("Nós precisamos dessa permissão.");
-        return;
-      }
-    }
-
-    const data = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All
-    });
-
-    if (data.cancelled) {
-      return;
-    }
-
-    if (!data.uri) {
-      return;
-    }
-
-    setAvatar(data);
-  }
-
-  async function uploadImage() {
-    const data = new FormData();
-
-    data.append("avatar", {
-      uri: avatar.uri,
-      type: avatar.type
-    });
-
-    await Axios.post("http://localhost:3333/files", data);
-  }
-
+export default function App() { 
   return (
+
     <View style={styles.container}>
-        <TouchableOpacity style={styles.addImageButton} onPress={uploadImage}>
+        <TouchableOpacity style={styles.addImageButton} onPress={() => {}} >
           <Text style={{ color: 'white', fontSize: 20, fontWeight:"bold", marginTop: -2}}>+</Text>
         </TouchableOpacity>
     </View> 
